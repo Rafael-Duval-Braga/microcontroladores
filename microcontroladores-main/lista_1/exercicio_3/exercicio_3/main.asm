7dad7810fab0e565221f92d3cbdfa2acfb711dc0
Rafael Braga <rafael.braga@ecomp.ufsm.br>
2025-04-20 20:23:07 -0300

    Update main.asm

--- microcontroladores-main/lista_1/exercicio_3/exercicio_3/main.asm
+++ microcontroladores-main/lista_1/exercicio_3/exercicio_3/main.asm
@@ -1,24 +1,23 @@
-; Programa que lê dois valores de PORTB e PORTC e mostra a soma em PORTD
+; Programa que lÃª dois valores de PORTB e PORTC e mostra a soma em PORTD
 
 start:
 	; Limpa os registradores usados
 	clr r16
-	clr r17
 
 	; Configura PORTB e PORTC como entrada
 	out DDRB, r16
 	out DDRC, r16
 
-	; Configura PORTD como saída
-	ser r18          ; r18 = 0xFF
-	out DDRD, r18
+	; Configura PORTD como saÃ­da
+	ser r17          ; r17 = 0xFF
+	out DDRD, r17
 
 	; Ativa pull-ups em PORTB e PORTC
-	out PORTB, r18
-	out PORTC, r18
+	out PORTB, r16
+	out PORTC, r16
 
 loop:
-	; Lê os valores de entrada
+	; LÃª os valores de entrada
 	in r16, PINB
 	in r17, PINC
 
